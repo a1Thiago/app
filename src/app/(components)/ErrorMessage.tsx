cd46ad94ed17c9627dd3f1eba7cf717993319f38
a1Thiago~ <a1thiago@hotmail.com>
2023-06-25 16:56:20 -0300

    best error component

--- src/app/(components)/ErrorMessage.tsx
+++ src/app/(components)/ErrorMessage.tsx
@@ -5,10 +5,16 @@ interface ErrorProps {
 }
 
 export default function ErrorMessage({ error }: ErrorProps) {
+
+  const errorStatusRegex = /(?<=\()\d+(?=\))/g
+
+  const errorStatus = error?.match(errorStatusRegex)
+
   return (
-    <div className='grid gap-8  place-items-center text-center p-4'>
-      <h3 className='text-32 tablet:text-24 mobile:text-20 font-medium mt-32'>{error}</h3>
+    <div className='mt-32 grid gap-2 place-items-center text-center p-4'>
+      <p className='text-24 tablet:text-20 mobile:text-16'>{error}</p>
       <Image src={'/error.png'} alt='error image' width={100} height={100} />
+      {errorStatus && (<h3 className='text-32 tablet:text-24 mobile:text-20 font-bold text-red-500'>{errorStatus}</h3>)}
     </div>
   )
 }
