a0b3f4ac832f5f5efdb5598d9c2d038ba93b8abd
a1Thiago~ <a1thiago@hotmail.com>
2023-06-25 07:44:35 -0300

    fixed button props

--- src/app/(components)/Button.tsx
+++ src/app/(components)/Button.tsx
@@ -1,12 +1,11 @@
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
-  onClick?: () => void
 }
 
-export default function Button({ label, onClick, ...props }: ButtonProps) {
+export default function Button({ label, ...props }: ButtonProps) {
   return (
-    <button onClick={onClick} className="disabled:opacity-60 disabled:cursor-not-allowed cursor-pointer text-center truncate inline-block
+    <button className="disabled:opacity-60 disabled:cursor-not-allowed cursor-pointer text-center truncate inline-block
      bg-theme-secondary text-white font-semibold mx-2 px-4 py-2 rounded hover:opacity-90 transition-opacity" {...props}>
       {label}
     </button>
