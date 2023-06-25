8df5d508908ef08ab678ff16f7bdc692db2ca892
a1Thiago~ <a1thiago@hotmail.com>
2023-06-25 07:43:15 -0300

    fixed button margin

--- src/app/(components)/Button.tsx
+++ src/app/(components)/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export default function Button({ label, onClick, ...props }: ButtonProps) {
   return (
     <button onClick={onClick} className="disabled:opacity-60 disabled:cursor-not-allowed cursor-pointer text-center truncate inline-block
-     bg-theme-secondary text-white font-semibold ml-2 px-4 py-2 rounded hover:opacity-90 transition-opacity" {...props}>
+     bg-theme-secondary text-white font-semibold mx-2 px-4 py-2 rounded hover:opacity-90 transition-opacity" {...props}>
       {label}
     </button>
   )
