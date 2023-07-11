c0def81e9bfe74d32de5d4e3ac2444666a45b6c3
a1Thiago~ <a1thiago@hotmail.com>
2023-07-11 01:42:09 -0300

    no

--- src/app/(components)/ErrorMessage.tsx
+++ src/app/(components)/ErrorMessage.tsx
@@ -1,14 +1,10 @@
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
-import Button from './Button'
 
 interface ErrorProps {
   error: string
 }
 
 export default function ErrorMessage({ error }: ErrorProps) {
-
-  const router = useRouter()
 
   const errorStatusRegex = /(?<=\()\d+(?=\))/g
 
@@ -19,9 +15,6 @@ export default function ErrorMessage({ error }: ErrorProps) {
       <p className='text-24 tablet:text-20 mobile:text-16'>{error}</p>
       <Image src={'/error.png'} alt='error image' width={100} height={100} />
       {errorStatus && (<h3 className='text-32 tablet:text-24 mobile:text-20 font-bold text-red-500'>{errorStatus}</h3>)}
-      <span className='mt-4'>
-        <Button colorStyle='secondary' label='Click aqui para tentar novamente' onClick={() => router.refresh()} />
-      </span>
     </div>
   )
 }
