8365752766c06137232b8cfadf2c3f7b8bcdf24c
a1Thiago~ <a1thiago@hotmail.com>
2023-07-11 13:01:37 -0300

    change to children

--- src/app/(components)/Button.tsx
+++ src/app/(components)/Button.tsx
@@ -1,6 +1,6 @@
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode
+  children?: React.ReactNode
   colorStyle?: 'primary' | 'secondary'
 }
 
