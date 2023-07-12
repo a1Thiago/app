ee82fbd7cf6ace346b40f104fb2e5e0a3384015f
a1Thiago~ <a1thiago@hotmail.com>
2023-07-12 09:53:14 -0300

    sticky bottom

--- src/app/(components)/LayoutFooter.tsx
+++ src/app/(components)/LayoutFooter.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function LayoutFooter() {
   return (
-    <footer className='mt-auto bg-theme-secondary-dark text-white text-center py-2 font-semibold'>
+    <footer className='mt-auto bg-theme-secondary-dark text-white text-center py-1 font-semibold sticky bottom-0'>
       <Link href={'https://github.com/a1Thiago/app'} target='_blank' >
         GitHub
       </Link>
