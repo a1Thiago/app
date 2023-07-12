521529cc0c2234e87b3219099a97a9bb24d4981d
a1Thiago~ <a1thiago@hotmail.com>
2023-07-11 23:58:20 -0300

    header color

--- src/app/(components)/LayoutHeader.tsx
+++ src/app/(components)/LayoutHeader.tsx
@@ -4,7 +4,7 @@ import HeaderButtons from './HeaderButtons'
 
 export default function LayoutHeader() {
   return (
-    <header className='px-4 py-2 bg-theme-primary'>
+    <header className='px-4 py-2 bg-gradient-to-r from-theme-secondary to-theme-primary/70'>
       <div className='max-w-[1440px] mx-auto'>
         <div className='grid grid-cols-2 items-center'>
           <Link href={'/'}>
