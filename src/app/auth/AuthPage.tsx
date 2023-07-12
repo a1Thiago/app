a19fd25ce506cae0735dbeab6ff6e81d6b1c55d6
a1Thiago~ <a1thiago@hotmail.com>
2023-07-12 11:26:49 -0300

    AuthPage

--- src/app/auth/AuthPage.tsx
+++ src/app/auth/AuthPage.tsx
@@ -1,4 +1,6 @@
 import AuthWrapper from '@/app/@authModal/(.)auth/AuthWrapper'
+import Link from 'next/link'
+import BackButton from '../(components)/BackButton'
 
 interface AuthPageProps {
   children: React.ReactNode
@@ -9,7 +11,10 @@ export default function AuthPage({ children }: AuthPageProps) {
 
     <section className='absolute bottom-0 inset-0 h-screen w-screen flex flex-col items-center justify-center
     bg-gradient-to-b from-theme-secondary-dark via-theme-dark to-black'>
-      <div className='shadow-lg shadow-theme-primary'>
+      <span className='top-4 left-4 absolute'>
+        <BackButton className='text-white' />
+      </span>
+      <div className='shadow-md shadow-theme-primary'>
         <AuthWrapper>
           {children}
         </AuthWrapper>
