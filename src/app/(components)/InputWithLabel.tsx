33f08d8a664a8bc8d7a159531f29ec786ab9c004
a1Thiago~ <a1thiago@hotmail.com>
2023-07-13 10:51:45 -0300

    fix

--- src/app/(components)/InputWithLabel.tsx
+++ src/app/(components)/InputWithLabel.tsx
@@ -1,9 +1,8 @@
-import { useGameStore } from '@/contexts/gameStore'
-import { Envelope, Lock, IdentificationBadge } from '@phosphor-icons/react'
+import { Envelope, Lock } from '@phosphor-icons/react'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string
-  icon?: 'userName' | 'password' | 'email'
+  icon?: 'password' | 'email'
   inputRef?: React.RefObject<HTMLInputElement>
 }
 
@@ -14,8 +13,7 @@ export default function InputWithLabel({ label, icon, inputRef, ...props }: Inpu
       <label className="mb-2 text-16 font-medium" htmlFor={label}>
         {label}
       </label>
-      <div className="flex gap-2 ring-theme-primary-dark  ring-1 items-center rounded px-2 h-10 bg-theme-primary/50 focus-within:ring-2  focus-within:text-black text-black/80">
-        {icon === 'userName' && <IdentificationBadge />}
+      <div className=" flex gap-2 ring-theme-primary-dark  ring-1 items-center rounded px-2 h-10 bg-theme-primary/50 focus-within:ring-2  focus-within:text-black text-black/80">
         {icon === 'password' && <Lock />}
         {icon === 'email' && <Envelope />}
         <input className="bg-transparent outline-none flex-1 placeholder:text-black/50" ref={inputRef}
