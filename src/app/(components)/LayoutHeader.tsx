840543ef0029febf3b2fc99dc018c4a785be1b7b
a1Thiago~ <a1thiago@hotmail.com>
2023-07-15 02:04:21 -0300

    new logo

--- src/app/(components)/LayoutHeader.tsx
+++ src/app/(components)/LayoutHeader.tsx
@@ -6,11 +6,10 @@ export default function LayoutHeader() {
   return (
     <header className='px-4 py-2 bg-gradient-to-r from-theme-secondary to-theme-primary/70'>
       <div className='max-w-[1440px] mx-auto'>
-        <div className='grid grid-cols-2 items-center'>
-          <Link href={'/'}>
-            <Image src={'/logo2.svg'} alt='a1Th logo' width={100} height='0' className='-my-8' />
+        <div className='grid grid-cols-2 mobile:grid-cols-1 justify-center items-center'>
+          <Link className='grid mobile:hidden items-center h-12 w-12  relative' href={'/'}>
+            <Image src={'/logo.png'} alt='a1Th logo' quality={100} fill className='transition-all hover:scale-125' />
           </Link>
-          {/* TODO FIX THE LOGOS FILES */}
           <span className='place-self-end min-w-[240px]'>
             <HeaderButtons />
           </span>
