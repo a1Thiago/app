e463ad92dcb511a2ebdd5897020b6d90e65e6e02
a1Thiago~ <a1thiago@hotmail.com>
2023-07-15 21:02:13 -0300

    change github link

--- src/app/(components)/LayoutFooter.tsx
+++ src/app/(components)/LayoutFooter.tsx
@@ -2,8 +2,10 @@ import Link from 'next/link'
 
 export default function LayoutFooter() {
   return (
-    <footer className='z-40 mt-auto bg-theme-secondary-dark text-white text-center py-1 font-semibold sticky bottom-0'>
-      <Link href={'https://github.com/a1Thiago/app'} target='_blank' >
+    <footer className='z-40 mt-auto bg-theme-secondary-dark text-white text-center py-1 font-semibold 
+    sticky- bottom-0-
+    '>
+      <Link href={'https://github.com/a1Thiago'} target='_blank' >
         GitHub
       </Link>
     </footer>
